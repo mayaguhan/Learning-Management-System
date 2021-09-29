1e668b8c0e300573992d8cbce6fa26c187882da6
David Lim <73053181+davidlyj@users.noreply.github.com>
2021-09-29 21:57:15 +0800

    Update Quiz.vue page and set up props for the pages
    Set up dummy data template

--- src/views/Learner/Quiz.vue
+++ src/views/Learner/Quiz.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="mt-5" style="text-align: center">
-        <h1>Quiz Page</h1>
+        <h1>{{section_id}} Quiz</h1>
     </div>
 </template>
 
 <script>
 export default {
     name: "Quiz",
+    props: {
+        course_id: parseInt({ type: Number }), 
+        learner_id: parseInt({ type: Number }),
+        section_id: parseInt({ type: Number }),
+    },
     components: {
         //
     },
@@ -14,10 +19,16 @@ export default {
         //
     },
     methods: {
-        //
+        // SELECT qq.quiz_question_id, qo.quiz_option_id, qq.question_name, qq.type, qo.option, qo.correct
+        // Get all Quiz Question and Quiz Option by section_id
+        
     },
     computed() {
         //
+    },
+    created() {
+        // Calls method to get quiz details
+        // this.getQuizDetail();
     }
 }
 </script>
