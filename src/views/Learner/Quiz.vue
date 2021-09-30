6709c58835bc5bf81d24bfb589b5e64201d9fa64
David Lim <73053181+davidlyj@users.noreply.github.com>
2021-09-30 15:25:06 +0800

    Update Quiz.vue
    Added dummy data

--- src/views/Learner/Quiz.vue
+++ src/views/Learner/Quiz.vue
@@ -15,20 +15,34 @@ export default {
     components: {
         //
     },
-    data() {
-        //
-    },
+    data: () => ({
+        quizDetailList: [],
+    }),
     methods: {
         // SELECT qq.quiz_question_id, qo.quiz_option_id, qq.question_name, qq.type, qo.option, qo.correct
         // Get all Quiz Question and Quiz Option by section_id
-        
+        getQuizDetail() {
+            let quizDetail = {
+                'quiz_question_id': 1,
+                'quiz_option_id': 1,
+                'question_name': 'Lorem ipsum dolor sit amet, consectetur adipiscing...',
+                'type': 'MCQ',
+                'option': 'Ut auctor augue varius vestibulum.',
+                'correct': 1,
+            };
+            this.quizDetailList.push(quizDetail);
+        },
     },
-    computed() {
-        //
+    computed: {
+        apiLink(){
+            return this.$store.state.apiLink;
+        }
     },
     created() {
         // Calls method to get quiz details
         // this.getQuizDetail();
+        this.getQuizDetail();
+        
     }
 }
 </script>
