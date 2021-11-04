3e04a2ab70c37f1d5870de1e55885da2856020a1
mayaguhan <i.am.maya.guhan@gmail.com>
2021-11-05 00:22:47 +0800

    Deleted getter method

--- backend/api/data_access/lms_quiz_choice.py
+++ backend/api/data_access/lms_quiz_choice.py
@@ -16,9 +16,6 @@ class LMSQuizChoice(db.Model):
     def getChoiceID(self):
         return self.quiz_choice_id
     
-    def getCorrect(self):
-        return self.correct
-    
     def setChoiceID(self,newID):
         self.quiz_choice_id = newID
 
